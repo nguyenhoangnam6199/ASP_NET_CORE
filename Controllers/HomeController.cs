900f4df0af18acdb5f5820cfac70b1d322c03c85
nguyenhoangnam6199 <57783011+nguyenhoangnam6199@users.noreply.github.com>
2020-08-28 16:21:18 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Buoi3_ASP.Models;
+
+namespace Buoi3_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Demo()
+        {
+            return View();
+        }
+
+        public IActionResult SanPham()
+        {
+            Random rd = new Random();
+            int sl = rd.Next(4, 100);
+            List<HangHoa> dsHangHoa = new List<HangHoa>();
+
+            for(int i= 0; i<sl; i++)
+            {
+                dsHangHoa.Add(new HangHoa
+                {
+                    TenHH = $"SamSung {rd.Next()}",
+                    DonGia = rd.Next(1000,100000),
+                    GiamGia = rd.NextDouble()
+                });
+            }
+            return View(dsHangHoa);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
