57ca10a88f49ff840efa3059f67f3d3aab2fecce
nguyenhoangnam6199 <57783011+nguyenhoangnam6199@users.noreply.github.com>
2020-08-29 13:12:08 +0700

    Add project files.

---
+++ Buoi10_Validation_Layout/Controllers/EmployeeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Buoi10_Validation_Layout.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi10_Validation_Layout.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Employee employee)
+        {
+            return View();
+        }
+
+        public IActionResult CheckEmployeeExist(string EmployeeNo)
+        {
+            var accounts = new List<string>()
+            {
+                "admin", "guest", "abc"
+            };
+
+            if(accounts.Contains(EmployeeNo))
+            {
+                return Json("Mã này đã tồn tại !");
+            }
+
+            return Json(true);
+        }
+
+        public IActionResult Register()
+        {
+            var rd = new Random();
+            var pattern = @"0123456789cjdfjgfgfgfnbfnvkfnvnsdsjf";
+            // var rdCode = rd.Next(1000, 10000).ToString();
+            //StringBuilder dùng để cộng các chuỗi lại với nhau, không nên dùng (string + string)
+            var rdCode = new StringBuilder();
+            for(var i = 0; i<6; i++)
+            {
+                rdCode.Append(pattern[rd.Next(0, pattern.Length)]);
+            }
+            
+            ViewBag.RandomCode = rdCode;
+
+            HttpContext.Session.SetString("SecurityCode", rdCode.ToString());
+
+            return View();
+        }
+
+        public IActionResult CheckSecurityCode(string MaBaoMat)
+        {
+            if(MaBaoMat == HttpContext.Session.GetString("SecurityCode"))
+            {
+                return Content("true");
+            }
+            else return Content("false");
+        }
+
+        public IActionResult NoView()
+        {
+            return View();
+        }
+
+          [HttpGet("/Product")]
+          public IActionResult ShowProduct()
+            {
+                return View();
+            }
+
+       
+    }
+}
---
+++ Buoi10_Validation_Layout/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi10_Validation_Layout.Models
+{
+    public class Employee
+    {
+        [Display(Name ="Mã nhân viên")]
+        [Required(ErrorMessage = "*")]
+        [Remote(action: "CheckEmployeeExist", controller: "Employee")]
+        public string EmployeeNo { get; set; }
+
+        [RegularExpression("^0[5789][0-9]{8}$",ErrorMessage ="Số điện thoại không đúng định dạng !")]
+        public string Phone { get; set; }
+
+        [RegularExpression(@"^\w([.+-]\w+)*@\w{2,}([.]\w{2,4})+$",ErrorMessage ="Chưa đúng định dạng email !")]
+        public string Email { get; set; }
+    }
+}
