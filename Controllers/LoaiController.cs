d3386ee9cfb4e12966ef78be3a6128e726dd388b
nguyenhoangnam6199 <57783011+nguyenhoangnam6199@users.noreply.github.com>
2020-09-03 19:54:37 +0700

    Update 1

---
+++ Controllers/LoaiController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Buoi23_DemoReactAPI.Models;
+
+namespace Buoi23_DemoReactAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaiController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public LoaiController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Loai
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Loai>>> GetLoais()
+        {
+            return await _context.Loais.ToListAsync();
+        }
+
+        // GET: api/Loai/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Loai>> GetLoai(int id)
+        {
+            var loai = await _context.Loais.FindAsync(id);
+
+            if (loai == null)
+            {
+                return NotFound();
+            }
+
+            return loai;
+        }
+
+        // PUT: api/Loai/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLoai(int id, Loai loai)
+        {
+            if (id != loai.MaLoai)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(loai).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoaiExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Loai
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Loai>> PostLoai(Loai loai)
+        {
+            _context.Loais.Add(loai);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLoai", new { id = loai.MaLoai }, loai);
+        }
+
+        // DELETE: api/Loai/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Loai>> DeleteLoai(int id)
+        {
+            var loai = await _context.Loais.FindAsync(id);
+            if (loai == null)
+            {
+                return NotFound();
+            }
+
+            _context.Loais.Remove(loai);
+            await _context.SaveChangesAsync();
+
+            return loai;
+        }
+
+        private bool LoaiExists(int id)
+        {
+            return _context.Loais.Any(e => e.MaLoai == id);
+        }
+    }
+}
