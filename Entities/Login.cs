b18181a505cd4dd5431526822adaa06a0bec3403
nguyenhoangnam6199 <57783011+nguyenhoangnam6199@users.noreply.github.com>
2020-09-03 15:53:28 +0700

    Add project files.

---
+++ Entities/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi22_WebAPI.Entities
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ViewModels/VHangHoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Buoi22_WebAPI.ViewModels
+{
+    public partial class VHangHoa
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public int MaLoai { get; set; }
+        public string MoTaDonVi { get; set; }
+        public double? DonGia { get; set; }
+        public string Hinh { get; set; }
+        public DateTime NgaySx { get; set; }
+        public double GiamGia { get; set; }
+        public int SoLanXem { get; set; }
+        public string MoTa { get; set; }
+        public string MaNcc { get; set; }
+        public string TenLoai { get; set; }
+        public string TenNhaCungCap { get; set; }
+    }
+}
