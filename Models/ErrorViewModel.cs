130932cdb2fe6d98fbb2ea5e8e9e56d6640b110c
nguyenhoangnam6199 <57783011+nguyenhoangnam6199@users.noreply.github.com>
2020-09-04 12:38:18 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Buoi11_ASP_LayOut.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
