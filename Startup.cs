fc83f001c0f8e58ed91b41d72dfedb4fce1cc44f
nguyenhoangnam6199 <57783011+nguyenhoangnam6199@users.noreply.github.com>
2020-09-08 18:43:22 +0700

    them chuc nang

--- Startup.cs
+++ Startup.cs
@@ -56,6 +56,10 @@ namespace DoAn
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
