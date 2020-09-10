bf81eef9eb06e068c89a260e09e1cdf56ab45448
nguyenhoangnam6199 <57783011+nguyenhoangnam6199@users.noreply.github.com>
2020-09-10 18:32:28 +0700

    Add fie helper

--- Data/HangHoa.cs
+++ Data/HangHoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,17 +9,26 @@ namespace DoAn.Data
 {
     public class HangHoa
     {
+        [Display(Name ="Mã")]
         public Guid MaHangHoa { get; set; }
+        [Display(Name = "Tên hàng hóa")]
         public string TenHangHoa { get; set; }
+        [Display(Name = "Đơn giá")]
         public double DonGia { get; set; }
+        [Display(Name = "Giảm giá")]
         public byte GiamGia { get; set; }
+        [Display(Name = "Số lượng")]
         public int SoLuong { get; set; }
+        [Display(Name = "Hình")]
         public string Hinh { get; set; }
+        [Display(Name = "Chi tiết")]
         public string ChiTiet { get; set; }
+        [Display(Name = "Mô tả")]
         public string MoTa { get; set; }
+        
 
         public int? MaLoai { get; set; }
-
+        [Display(Name = "Loại")]
         public Loai Loai { get; set; }
 
         public virtual ICollection<HangHoaTag> HangHoaTags { get; set; }
@@ -26,7 +36,7 @@ namespace DoAn.Data
         public virtual ICollection<HinhPhu> HinhPhus { get; set; }
 
         public virtual ICollection<ReviewHangHoa> ReviewHangHoas { get; set; }
-
+        [Display(Name = "Số sao bình chọn")]
         public double? DiemReview { get; set; }
     }
 
---
+++ Helpers/FileHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn.Helpers
+{
+    public class FileHelper
+    {
+        public static string UpLoadFileToFolder(IFormFile file, string folderName)
+        {
+            try
+            {
+                var fileName = $"{DateTime.Now.Ticks}_{file.FileName}";
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Hinh", folderName, fileName);
+                using (var myFile = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(myFile);
+                }
+                return fileName;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
+
