d4b7f02553317b48e0f5aa48e5bbe6a70933904b
nguyenhoangnam6199 <57783011+nguyenhoangnam6199@users.noreply.github.com>
2020-09-12 20:18:57 +0700

    up phần category

--- ViewComponents/CategoryMenu.cs
+++ ViewComponents/CategoryMenu.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using DoAn.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DoAn.ViewComponents
 {
-    public class CategoryMenu
+    public class CategoryMenu:ViewComponent
     {
+        private readonly MyDbContext _context;
+        public CategoryMenu(MyDbContext context)
+        {
+            _context = context;
+        }
 
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _context.Loais.ToListAsync());
+        }
     }
 }
