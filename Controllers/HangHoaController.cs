cc884dbc1ca59e4c74d9c34683cf7683ed4b48eb
nguyenhoangnam6199 <57783011+nguyenhoangnam6199@users.noreply.github.com>
2020-09-15 16:29:46 +0700

    ôn lại

---
+++ Controllers/HangHoaController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DoAn.Data;
+using DoAn.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+
+namespace DoAn.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly MyDbContext context;
+        public HangHoaController(MyDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index(int? MaLoai)
+        {
+            var data = context.HangHoas.AsQueryable();
+            if (MaLoai.HasValue)
+            {
+                data = data.Where(hh => hh.MaLoai == MaLoai || hh.Loai.MaLoaiCha == MaLoai);
+
+                //C2: Xác đinh danh sách loại cha
+                //List<int> dsLoai = LayDanhSachLoai(MaLoai);
+                //data = data.Where(hh => dsLoai.Contains(hh.MaLoai.Value));
+            }
+
+            var dshangHoa = data.Select(hh => new HangHoaVM
+            {
+                MaHangHoa = hh.MaHangHoa,
+                TenHangHoa = hh.TenHangHoa,
+                DonGia = hh.DonGia,
+                GiamGia = hh.GiamGia,
+                Hinh = hh.Hinh,
+                MoTa = hh.MoTa,
+                SoLuong = hh.SoLuong,
+                TenLoai = hh.Loai.TenLoai
+            }).ToList();
+            return View(dshangHoa);
+        }
+
+        private List<int> LayDanhSachLoai(int? maLoai)
+        {
+            var danhsach = new List<int>();
+
+            return danhsach;
+        }
+
+        public void DeQuyTimLoai(int maLoai, List<int> ds)
+        {
+            ds.Add(maLoai);
+            var loaiCon = context.Loais.Where(lo => lo.MaLoaiCha == maLoai)
+                .Select(lo=>lo.MaLoai)
+                .ToList();
+            while(loaiCon.Any())
+            {
+                var obj = loaiCon.First();
+                loaiCon.Remove(obj);
+                DeQuyTimLoai(obj, ds);
+            }
+        }
+    }
+}
---
+++ ViewModels/HangHoaVM.cs
@@ -0,0 +1,32 @@
+﻿using DoAn.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn.ViewModels
+{
+    public class HangHoaVM
+    {
+        [Display(Name = "Mã")]
+        public Guid MaHangHoa { get; set; }
+        [Display(Name = "Tên hàng hóa")]
+        public string TenHangHoa { get; set; }
+        [Display(Name = "Đơn giá")]
+        public double DonGia { get; set; }
+        [Display(Name = "Giảm giá")]
+        public byte GiamGia { get; set; }
+        [Display(Name = "Giá bán")]
+        public double GiaBan => DonGia * (100 - GiamGia) / 100.0;
+        [Display(Name = "Số lượng")]
+        public int SoLuong { get; set; }
+        [Display(Name = "Hình")]
+        public string Hinh { get; set; }
+        [Display(Name = "Mô tả")]
+        public string MoTa { get; set; }
+
+        [Display(Name = "Loại")]
+        public string TenLoai { get; set; }
+    }
+}
